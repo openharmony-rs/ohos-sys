a02710adbecbb92c46fda75efdb34f731d44d462
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-07-15 17:54:53 +0200

    Generate napi API-11 bindings
    Signed-off-by: Jonathan Schwender <jonathan.schwender@huawei.com>

--- src/napi.rs
+++ src/napi.rs
@@ -3,3 +3,10 @@ extern "C" {}
 
 mod napi_api10;
 pub use napi_api10::*;
+
+#[cfg(feature = "api-11")]
+#[cfg_attr(docsrs, doc(cfg(feature = "api-11")))]
+mod api11_additions;
+#[cfg(feature = "api-11")]
+#[cfg_attr(docsrs, doc(cfg(feature = "api-11")))]
+pub use api11_additions::*;
