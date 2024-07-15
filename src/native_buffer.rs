51c3e84d7df541c61082158caae1e7c0469dc316
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-07-15 17:55:23 +0200

    Generate native_buffer API-11 bindings
    Signed-off-by: Jonathan Schwender <jonathan.schwender@huawei.com>

--- src/native_buffer.rs
+++ src/native_buffer.rs
@@ -3,3 +3,11 @@ extern "C" {}
 
 mod native_buffer_api10;
 pub use native_buffer_api10::*;
+
+#[cfg(feature = "api-11")]
+#[cfg_attr(docsrs, doc(cfg(feature = "api-11")))]
+mod api11_additions;
+
+#[cfg(feature = "api-11")]
+#[cfg_attr(docsrs, doc(cfg(feature = "api-11")))]
+pub use api11_additions::*;
