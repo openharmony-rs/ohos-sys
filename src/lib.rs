29f97a02838f0826ff97292f3e102c3e87f0958b
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-07-15 10:43:03 +0200

    Feature-gate components

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
+#[cfg(feature = "hilog")]
+#[cfg_attr(docsrs, doc(cfg(feature = "hilog")))]
 pub mod hilog;
+
+#[cfg(feature = "napi")]
+#[cfg_attr(docsrs, doc(cfg(feature = "napi")))]
 pub mod napi;
+#[cfg(feature = "native_buffer")]
+#[cfg_attr(docsrs, doc(cfg(feature = "native_buffer")))]
 pub mod native_buffer;
+#[cfg(feature = "native_window")]
+#[cfg_attr(docsrs, doc(cfg(feature = "native_window")))]
 pub mod native_window;
+#[cfg(feature = "xcomponent")]
+#[cfg_attr(docsrs, doc(cfg(feature = "xcomponent")))]
 pub mod xcomponent;
