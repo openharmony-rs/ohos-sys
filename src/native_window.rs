f67696c6aa11b8d0f2d7f4250944d4a407250f4c
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-07-15 18:24:52 +0200

    Add native_window API-11 documentation
    Signed-off-by: Jonathan Schwender <jonathan.schwender@huawei.com>

--- src/native_window.rs
+++ src/native_window.rs
@@ -7,3 +7,10 @@ pub use native_window_api10::*;
 
 mod native_window_api10_fixup;
 pub use native_window_api10_fixup::NativeWindowOperation;
+
+#[cfg(feature = "api-11")]
+#[cfg_attr(docsrs, doc(cfg(feature = "api-11")))]
+mod api11_additions;
+#[cfg(feature = "api-11")]
+#[cfg_attr(docsrs, doc(cfg(feature = "api-11")))]
+pub use api11_additions::*;
