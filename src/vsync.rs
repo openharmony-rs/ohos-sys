8701a0dcff5b9cf0f522498445ed580632b6ad72
Jonathan Schwender <schwenderjonathan@gmail.com>
2024-11-06 19:43:09 +0100

    vsync: Update bindings to OH 5.0 release.
    Signed-off-by: Jonathan Schwender <jonathan.schwender@huawei.com>
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- src/vsync.rs
+++ src/vsync.rs
@@ -5,3 +5,10 @@ extern "C" {}
 
 mod vsync_api10;
 pub use vsync_api10::*;
+
+#[cfg(feature = "api-12")]
+#[cfg_attr(docsrs, doc(cfg(feature = "api-12")))]
+mod api12_additions;
+#[cfg(feature = "api-12")]
+#[cfg_attr(docsrs, doc(cfg(feature = "api-12")))]
+pub use api12_additions::*;
