c89ad6732db1282fde1f5a22ebedd3ee43c571ac
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-11-11 13:58:54 +0100

    Fix documentation for Native Image Kit
    Signed-off-by: Jonathan Schwender <jonathan.schwender@huawei.com>

--- components/multimedia/image_framework/src/lib.rs
+++ components/multimedia/image_framework/src/lib.rs
@@ -11,6 +11,13 @@
 //!
 //! [Introduction to Image Kit]: https://docs.openharmony.cn/pages/v5.0/en/application-dev/media/image/image-overview.md
 //! [Image NativeModule]: https://docs.openharmony.cn/pages/v5.0/en/application-dev/media/image/image-structure-c.md
+//!
+//! ## Feature flags
+#![cfg_attr(
+    feature = "document-features",
+    cfg_attr(doc, doc = ::document_features::document_features!())
+)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 #[cfg(feature = "api-12")]
 #[cfg_attr(docsrs, doc(cfg(feature = "api-12")))]
