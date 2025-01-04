542e7918483e9b275521cff9a6e796d5b901cf26
Jonathan Schwender <schwenderjonathan@gmail.com>
2025-01-04 13:21:49 +0100

    Generator: use nightly fmt options
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- scripts/generate_bindings.sh
+++ scripts/generate_bindings.sh
@@ -8,6 +8,6 @@ ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"/.. &> /dev/null && pwd )
 (cd "${ROOT_DIR}/scripts/generator" && cargo run )
 
 cd "${ROOT_DIR}"
-cargo fmt
-fd -e rs . "${ROOT_DIR}/src/" --exec rustfmt
-fd -e rs . "${ROOT_DIR}/components" --exec rustfmt
+cargo +nightly fmt
+fd -e rs . "${ROOT_DIR}/src/" --exec rustfmt +nightly
+fd -e rs . "${ROOT_DIR}/components" --exec rustfmt +nightly
