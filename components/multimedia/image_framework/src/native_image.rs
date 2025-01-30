33d5c3c0c1e1bbc02d606f5ad1dfa29a71d79fcd
Jonathan Schwender <schwenderjonathan@gmail.com>
2025-01-30 16:13:42 +0100

    image-packer: Properly feature guard
    The features weren't actually used.
    This is just a hot fix for CI.
    A more complete solution is still needed to address
    types being used across multiple components / modules.
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- components/multimedia/image_framework/src/native_image.rs
+++ components/multimedia/image_framework/src/native_image.rs
@@ -4,10 +4,18 @@
 
 pub mod common;
 pub mod image;
+#[cfg(feature = "image-packer")]
+#[cfg_attr(docsrs, doc(cfg(feature = "image-packer")))]
 pub mod image_packer;
+#[cfg(feature = "image-receiver")]
+#[cfg_attr(docsrs, doc(cfg(feature = "image-receiver")))]
 pub mod image_receiver;
+#[cfg(feature = "image-source")]
+#[cfg_attr(docsrs, doc(cfg(feature = "image-source")))]
 pub mod image_source;
-#[cfg(feature = "api-13")]
-#[cfg_attr(docsrs, doc(cfg(feature = "api-13")))]
+#[cfg(all(feature = "api-13", feature = "pixelmap"))]
+#[cfg_attr(docsrs, doc(cfg(all(feature = "api-13", feature = "pixelmap"))))]
 pub mod picture;
+#[cfg(feature = "pixelmap")]
+#[cfg_attr(docsrs, doc(cfg(feature = "pixelmap")))]
 pub mod pixelmap;
