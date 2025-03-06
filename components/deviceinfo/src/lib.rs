f89c9db2d9570824c78d5155bd1a0843b15501d8
Jonathan Schwender <schwenderjonathan@gmail.com>
2025-03-06 16:51:13 +0100

    deviceinfo: Link against deviceinfo_ndk.z
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- components/deviceinfo/src/lib.rs
+++ components/deviceinfo/src/lib.rs
@@ -1,4 +1,11 @@
 //! OpenHarmony functions providing device information.
+//!
+//! See also the official [DeviceInfo documentation].
+//!
+//! [DeviceInfo documentation]: https://docs.openharmony.cn/pages/v5.0/en/application-dev/reference/apis-basic-services-kit/_device_info.md
+
+#[link(name = "deviceinfo_ndk.z")]
+extern "C" {}
 
 mod deviceinfo_ffi;
 pub use deviceinfo_ffi::*;
