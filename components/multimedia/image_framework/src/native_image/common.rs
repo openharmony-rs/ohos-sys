72be9a07a5cda8f245c070ae4172308a0a8c2231
Jonathan Schwender <schwenderjonathan@gmail.com>
2026-02-19 22:12:16 +0800

    fix image framework link
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- components/multimedia/image_framework/src/native_image/common.rs
+++ components/multimedia/image_framework/src/native_image/common.rs
@@ -2,7 +2,7 @@
 //!
 //! <https://docs.openharmony.cn/pages/v5.0/en/application-dev/reference/apis-image-kit/image__common_8h.md>
 
-#[link(name = "image_common")]
+#[cfg_attr(feature = "api-13", link(name = "image_common"))]
 extern "C" {}
 
 #[path = "image_common/image_common_ffi.rs"]
